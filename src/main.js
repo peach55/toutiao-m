b4fa28883c3890ce866642b3cf7674bc03b7c8f1
‘peach55’ <709144426@qq.com>
2021-12-05 21:00:13 +0800

    初始化项目结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入Vant 组件及样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 导入全局样式
+import '@/styles/index.less'
 
 Vue.config.productionTip = false
+
+// 将 Vant 注册为 Vue 的插件
+Vue.use(Vant)
 
 new Vue({
   router,
